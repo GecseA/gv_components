f2c975baf3dd474545236afef704f69978f6e591
AstrA <gecsevar@gmail.com>
2023-12-15 19:32:11 +0100

    GV Checkbox & GV Select

---
+++ GVCheckbox.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import {getIn} from "formik";
+import Box from "@mui/material/Box";
+import {CheckboxProps} from "formik-mui";
+import {Checkbox, FormControlLabel, InputLabel} from "@mui/material";
+
+interface GVCheckboxProps extends CheckboxProps {
+    label: string
+}
+
+export function GVCheckbox({field, form: {errors, touched, isSubmitting, values, handleChange, setFieldValue}, ...props}: GVCheckboxProps) {
+
+    let errorMessage: any = undefined
+    if (getIn(touched, field.name) === true) {
+        errorMessage = getIn(errors, field.name)
+    }
+
+    return <Box display="flex" sx={{margin: "5px", alignItems: 'center' }}>
+        <FormControlLabel label={props.label} control={
+            <Checkbox
+                id={field.name}
+                name={field.name}
+                checked={typeof getIn(values, field.name) === "string" ?
+                    getIn(values, field.name) === "1" ? true : false
+                    :
+                    getIn(values, field.name)
+                }
+                onChange={(val) => {
+                    setFieldValue(field.name, val.target.checked)
+                }}
+            />
+        }/>
+    </Box>
+}
